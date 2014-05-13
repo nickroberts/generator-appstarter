79b3b43764c431eda4fad05180a1cc922d0a15e0
Nick Roberts <nick@utropicmedia.com>
2014-05-13 19:27:07 -0400

    Add gitignore, bowerrc, more prompts.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 'use strict';
-var util = require('util');
-var path = require('path');
+// var util = require('util');
+// var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
@@ -17,21 +17,149 @@ var AppstarterGenerator = yeoman.generators.Base.extend({
     });
   },
 
-  askFor: function () {
+  askAboutApp: function () {
     var done = this.async();
 
     // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Appstarter generator!'));
+    this.log(yosay('Let\'s gernate an app using the appstarter generator!'));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: 'appName',
+      message: 'What do you want to call your app?'
+    },{
+      name: 'appDescription',
+      message: 'Tell me a little bit about your app:'
     }];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      this.appName = props.appName;
+      this.appDescription = props.appDescription;
+
+      done();
+    }.bind(this));
+  },
+
+  askAboutFrameworks: function () {
+    var done = this.async();
+
+    var prompts = [
+    {
+      name: 'uxFramework',
+      type: 'list',
+      message: 'Would you like to include one of these wonderful UX frameworks?',
+      choices: ['None', 'Bootstrap', 'Foundation']
+    },{
+      name: 'jsFramework',
+      type: 'list',
+      message: 'Would you like to include one of these wonderful js frameworks?',
+      choices: ['None', 'Angularjs']
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.uxFramework  = props.uxFramework === 'None' ? false : props.uxFramework.toLowerCase();
+      this.jsFramework   = props.jsFramework === 'None' ? false : props.jsFramework.toLowerCase();
+
+      done();
+    }.bind(this));
+  },
+
+  askAboutPreprocessors: function () {
+    var done = this.async();
+    var uxFramework = this.uxFramework;
+
+    var prompts = [
+    {
+      name: 'cssPre',
+      type: 'list',
+      message: 'CSS preprocessor',
+      choices: ['None', 'Compass', 'Sass', 'Less'],
+      when: function () {
+        return true || !uxFramework;
+      }
+    },
+    {
+      name: 'jsPre',
+      type: 'list',
+      message: 'Javascript preprocessor',
+      choices: ['None', 'Coffeescript']
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.cssPre  = props.cssPre === 'None' ? false : props.cssPre.toLowerCase();
+      this.jsPre   = props.jsPre === 'None' ? false : props.jsPre.toLowerCase();
+
+      done();
+    }.bind(this));
+  },
+
+  askAboutDirectories: function () {
+    var done = this.async();
+    var cssPre = this.cssPre;
+    var jsPre  = this.jsPre;
+
+    var slashFilter = function (input) {
+      return input.replace(/^\/*|\/*$/g, '');
+    };
+
+    this.log(chalk.yellow('\nLet\'s set up some directories.'));
+
+    var prompts = [
+    {
+      name: 'cssPreDir',
+      message: 'CSS preprocessor directory',
+      default: this.cssPre === 'less' ? '_less' : '_scss',
+      filter: slashFilter,
+      when: function () {
+        return cssPre;
+      }
+    },
+    {
+      name: 'cssDir',
+      message: 'CSS directory',
+      default: 'css',
+      filter: slashFilter
+    },
+    {
+      name: 'jsPreDir',
+      message: 'Javascript preprocessor directory',
+      default: '_src',
+      filter: slashFilter,
+      when: function () {
+        return jsPre;
+      }
+    },
+    {
+      name: 'jsDir',
+      message: 'Javascript directory',
+      default: 'js',
+      filter: slashFilter
+    },
+    {
+      name: 'imgDir',
+      message: 'Image directory',
+      default: 'img',
+      filter: slashFilter
+    },
+    {
+      name: 'fontsDir',
+      message: 'Webfont directory',
+      default: 'fonts',
+      filter: slashFilter
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.cssDir    = props.cssDir;
+      this.jsDir     = props.jsDir;
+      this.imgDir    = props.imgDir;
+      this.fontsDir  = props.fontsDir;
+      this.cssPreDir = props.cssPreDir;
+      this.jsPreDir  = props.jsPreDir;
+
+      // Split asset directories on slashes
+      this.cssExDir   = props.cssDir.split('/').pop();
+      this.jsExDir    = props.jsDir.split('/').pop();
+      this.imgExDir   = props.imgDir.split('/').pop();
+      this.fontsExDir = props.fontsDir.split('/').pop();
 
       done();
     }.bind(this));
@@ -48,6 +176,8 @@ var AppstarterGenerator = yeoman.generators.Base.extend({
   projectfiles: function () {
     this.copy('editorconfig', '.editorconfig');
     this.copy('jshintrc', '.jshintrc');
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('gitignore', '.gitignore');
   }
 });
 
